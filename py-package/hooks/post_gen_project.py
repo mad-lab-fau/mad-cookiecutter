03ae9c2fe043f8163945b0dcdfbd9ee335344dfd
Arne Küderle <a.kuederle@gmail.com>
2023-03-21 11:02:58 +0100

    Checking identifier now

--- py-package/hooks/post_gen_project.py
+++ py-package/hooks/post_gen_project.py
@@ -11,16 +11,31 @@ def configure_venv_folder():
 
 def install_default_deps():
     """Install some basic deps.
-    
+
     We do that in the hook file to get the latest version for every new project.
     """
     dev_deps = ["black", "poethepoet", "pytest", "pytest-cov", "ruff"]
     doc_deps = ["numpydoc", "sphinx", "sphinx-gallery", "recommonmark", "memory-profiler", "matplotlib", "toml"]
 
-    subprocess.run(["poetry", "add", "--lock", "--dev", *dev_deps, *doc_deps])
+    subprocess.run(["poetry", "add", "--lock", "--group", "dev", *dev_deps, *doc_deps])
+
+
+def validate_project_slug():
+    """Validate the project slug.
+
+    We do that in the hook file to get the latest version for every new project.
+    """
+    project_slug = "{{ cookiecutter.project_slug }}"
+    if not project_slug.isidentifier():
+        raise ValueError(
+            f"Project slug '{project_slug}' is not a valid Python identifier. "
+            "A valid Python identifier is a non-empty string of letters, digits, and underscores."
+        )
 
 
 if __name__ == "__main__":
+    # Validate project slug
+    validate_project_slug()
     # Add custom configs
     configure_venv_folder()
     # Run Poetry check
